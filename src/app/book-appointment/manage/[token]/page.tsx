7f9fccff62d76d8158c83a9427a2134e06c77aad
heavycoderprem <136192975+heavycoderprem@users.noreply.github.com>
2025-05-30 03:36:01 +0530

    params fix

--- src/app/book-appointment/manage/[token]/page.tsx
+++ src/app/book-appointment/manage/[token]/page.tsx
@@ -1,6 +1,11 @@
 // This is a server component
 import BookingManager from './BookingManager';
 
-export default function BookingPage({ params }: { params: { token: string } }) {
-  return <BookingManager token={params.token} />;
+interface PageProps {
+  params: Promise<{ token: string }>;
+}
+
+export default async function BookingPage({ params }: PageProps) {
+  const { token } = await params;
+  return <BookingManager token={token} />;
 }
